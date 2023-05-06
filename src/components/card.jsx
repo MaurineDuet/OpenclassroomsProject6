4c7fea2b5cd603ca9f76d0eaaf57f30574cb9d93
Maurine Duet <maurineduet@gmail.com>
2023-05-06 17:55:25 +0200

    Début de la création de la page de logement

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,21 +1,24 @@
 import '../style/index.css'
 import '../style/home.css'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-function Card({title}) {
+function Card({ title }) {
       return (
+            <Link to="/accomodation">
                   <figure className="location-item">
                         <p>{title}</p>
                   </figure>
+            </Link>
       )
 }
 
 Card.propTypes = {
       title: PropTypes.string.isRequired,
-    }
-    
-    Card.defaultProps = {
+}
+
+Card.defaultProps = {
       title: '',
-    }
+}
 
 export default Card
--- src/pages/accomodation.js
+++ src/pages/accomodation.js
@@ -1,11 +1,24 @@
 import '../style/index.css'
+import '../style/accomodation.css'
+import Collapsible from '../components/collapse'
 
-function Accomodation () {
-    return(
-      <div>
-         <p>Chez vous, partout et ailleurs</p>
-      </div> 
-    )
+function Accomodation() {
+      return (
+            <div className="container">
+
+                  <div className="collapsible-container-acc">
+
+                        <Collapsible label="Description">
+                              <p className="collapsible-p"></p>
+                        </Collapsible>
+                        <Collapsible label="Equipements">
+                              <p className="collapsible-p"></p>
+                        </Collapsible>
+
+                  </div>
+
+            </div>
+      )
 }
 
- export default Accomodation
+export default Accomodation
